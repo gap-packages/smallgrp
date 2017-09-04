9eccd4bda0a9b73a2ab837edbd44f78308da0d68
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-09-04 09:43:23 +0100

    The package does not have any C code to build

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -12,9 +12,9 @@ then
 fi
 
 # build this package
-./autogen.sh
-./configure --with-gaproot=$GAPROOT
-make -j4 V=1
+# ./autogen.sh
+# ./configure --with-gaproot=$GAPROOT
+# make -j4 V=1
 
 # ... and link it into GAP pkg dir
 ls
