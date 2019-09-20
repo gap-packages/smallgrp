a3bd4df6d6c4989dfd81f59f08547e6ca6067cd8
Max Horn <max@quendi.de>
2019-09-20 14:45:43 +0200

    Add more manual examples
    Also, the example for UnloadSmallGroupsData really was a collection
    of general examples, so I moved some of them to more appropriate
    places, and deleted the rest.

--- gap/small.gd
+++ gap/small.gd
@@ -108,6 +108,16 @@ DeclareGlobalFunction( "SmallGroupsAvailable" );
 ##  <Description>
 ##  returns <C>true</C> if the number of groups of order <A>order</A> is known, and
 ##  <C>false</C> otherwise.
+##  <Example><![CDATA[
+##  gap> NumberSmallGroupsAvailable( 100 );
+##  true
+##  gap> NumberSmallGroups( 100 );
+##  16
+##  gap> NumberSmallGroupsAvailable( 4096 );
+##  false
+##  gap> NumberSmallGroups( 4096 );
+##  Error, the library of groups of size 4096 is not available
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -133,6 +143,20 @@ UnbindGlobal( "SmallGroup" );
 ##  otherwise it will be given as a permutation group.
 ##  If the groups of order <A>order</A> are not installed,
 ##  the function reports an error and enters a break loop.
+##  <Example><![CDATA[
+##  gap> G := SmallGroup( 60, 4 );
+##  <pc group of size 60 with 4 generators>
+##  gap> StructureDescription( G );
+##  "C60"
+##  gap> G := SmallGroup( 60, 5 );
+##  Group([ (1,2,3,4,5), (1,2,3) ])
+##  gap> StructureDescription( G );
+##  "A5"
+##  gap> G := SmallGroup( 768, 1000000 );
+##  <pc group of size 768 with 9 generators>
+##  gap> G := SmallGroup( [768, 1000000] );
+##  <pc group of size 768 with 9 generators>
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -189,6 +213,14 @@ UnbindGlobal( "AllGroups" );
 ##  <M>1024</M>, <M>1152</M>, <M>1536</M>, <M>1920</M> and those of order
 ##  <M>p^n \cdot q > 1000</M> 
 ##  with <M>n > 2</M>. 
+##  <Example><![CDATA[
+##  gap> AllSmallGroups( 6 );
+##  [ <pc group of size 6 with 2 generators>, 
+##    <pc group of size 6 with 2 generators> ]
+##  gap> AllSmallGroups( 60, IsNilpotentGroup );
+##  [ <pc group of size 60 with 4 generators>, 
+##    <pc group of size 60 with 4 generators> ]
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -212,6 +244,18 @@ UnbindGlobal( "OneGroup" );
 ##  returns one group with certain properties as specified by <A>arg</A>.
 ##  The permitted arguments are those supported by
 ##  <Ref Func="AllSmallGroups"/>.
+##  <Example><![CDATA[
+##  gap> G := OneSmallGroup( 6, IsAbelian );
+##  <pc group of size 6 with 2 generators>
+##  gap> StructureDescription( G );
+##  "C6"
+##  gap> G := OneSmallGroup( 6, IsAbelian, false );
+##  <pc group of size 6 with 2 generators>
+##  gap> StructureDescription( G );
+##  "S3"
+##  gap> G := OneSmallGroup( Size, [1..1000], IsSolvableGroup, false );
+##  Group([ (1,2,3,4,5), (1,2,3) ])
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -235,6 +279,16 @@ UnbindGlobal( "IdsOfAllGroups" );
 ##  similar to <C>AllSmallGroups</C> but returns ids instead of groups. This may
 ##  prevent workspace overflows, if a large number of groups are expected in 
 ##  the output.
+##  <Example><![CDATA[
+##  gap> IdsOfAllSmallGroups( 60, IsNilpotentGroup );
+##  [ [ 60, 4 ], [ 60, 13 ] ]
+##  gap> IdsOfAllSmallGroups( 60, IsNilpotentGroup, false );
+##  [ [ 60, 1 ], [ 60, 2 ], [ 60, 3 ], [ 60, 5 ], [ 60, 6 ], [ 60, 7 ], 
+##    [ 60, 8 ], [ 60, 9 ], [ 60, 10 ], [ 60, 11 ], [ 60, 12 ] ]
+##  gap> IdsOfAllSmallGroups( Size, 60, IsSupersolvableGroup, true );
+##  [ [ 60, 1 ], [ 60, 2 ], [ 60, 3 ], [ 60, 4 ], [ 60, 6 ], [ 60, 7 ], 
+##    [ 60, 8 ], [ 60, 10 ], [ 60, 11 ], [ 60, 12 ], [ 60, 13 ] ]
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -257,6 +311,14 @@ UnbindGlobal( "NumberSmallGroups" );
 ##
 ##  <Description>
 ##  returns the number of groups of order <A>order</A>.
+##  <Example><![CDATA[
+##  gap> NumberSmallGroups( 512 );
+##  10494213
+##  gap> NumberSmallGroups( 2^8 * 23 );
+##  1083472
+##  gap> NumberSmallGroups( 4096 );
+##  Error, the library of groups of size 4096 is not available
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -281,71 +343,7 @@ DeclareSynonym( "NrSmallGroups",NumberSmallGroups );
 ##  might wish to remove the data from the workspace and this can be done by 
 ##  the above function call.
 ##  <Example><![CDATA[
-##  gap> G := SmallGroup( 768, 1000000 );
-##  <pc group of size 768 with 9 generators>
-##  gap> G := SmallGroup( [768, 1000000] );
-##  <pc group of size 768 with 9 generators>
-##  gap> AllSmallGroups( 6 );
-##  [ <pc group of size 6 with 2 generators>, 
-##    <pc group of size 6 with 2 generators> ]
-##  gap> AllSmallGroups( Size, 120, IsSolvableGroup, false );
-##  [ Group(
-##      [ (1,2,4,8)(3,6,9,5)(7,12,13,17)(10,14,11,15)(16,20,21,24)(18,22,
-##          19,23), (1,3,7)(2,5,10)(4,9,13)(6,11,8)(12,16,20)(14,18,
-##          22)(15,19,23)(17,21,24) ]), Group([ (1,2,3,4,5), (1,2) ]), 
-##    Group([ (1,2,3,5,4), (1,3)(2,4)(6,7) ]) ]
-##  gap> G := OneSmallGroup( 120, IsNilpotentGroup, false );
-##  <pc group of size 120 with 5 generators>
-##  gap> IdSmallGroup(G);
-##  [ 120, 1 ]
-##  gap> G := OneSmallGroup( Size, [1..1000], IsSolvableGroup, false );
-##  Group([ (1,2,3,4,5), (1,2,3) ])
-##  gap> IdSmallGroup(G);
-##  [ 60, 5 ]
 ##  gap> UnloadSmallGroupsData();
-##  gap> IdSmallGroup( GL( 2,3 ) );
-##  [ 48, 29 ]
-##  gap> IdSmallGroup( Group( (1,2,3,4),(4,5) ) );
-##  [ 120, 34 ]
-##  gap> IdsOfAllSmallGroups( Size, 60, IsSupersolvableGroup, true );
-##  [ [ 60, 1 ], [ 60, 2 ], [ 60, 3 ], [ 60, 4 ], [ 60, 6 ], [ 60, 7 ], 
-##    [ 60, 8 ], [ 60, 10 ], [ 60, 11 ], [ 60, 12 ], [ 60, 13 ] ]
-##  gap> NumberSmallGroups( 512 );
-##  10494213
-##  gap> NumberSmallGroups( 2^8 * 23 );
-##  1083472
-##  ]]></Example>
-##  <P/>
-##  <Log><![CDATA[
-##  gap> NumberSmallGroups( 2^9 * 23 );
-##  Error, the library of groups of size 11776 is not available called from
-##  <function>( <arguments> ) called from read-eval-loop
-##  Entering break read-eval-print loop ...
-##  you can 'quit;' to quit to outer loop, or
-##  you can 'return;' to continue
-##  brk> quit;
-##  gap>
-##  ]]></Log>
-##  <P/>
-##  <Example><![CDATA[
-##  gap> SmallGroupsInformation( 32 );
-##  
-##    There are 51 groups of order 32.
-##    They are sorted by their ranks. 
-##       1 is cyclic. 
-##       2 - 20 have rank 2.
-##       21 - 44 have rank 3.
-##       45 - 50 have rank 4.
-##       51 is elementary abelian. 
-##  
-##    For the selection functions the values of the following attributes 
-##    are precomputed and stored:
-##       IsAbelian, PClassPGroup, RankPGroup, FrattinifactorSize and 
-##       FrattinifactorId. 
-##  
-##    This size belongs to layer 2 of the SmallGroups library. 
-##    IdSmallGroup is available for this size. 
-##   
 ##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
@@ -402,6 +400,12 @@ UnbindGlobal( "IdGroup" );
 ##  <Description>
 ##  returns the library number of <A>G</A>; that is, the function returns a pair
 ##  <C>[<A>order</A>, <A>i</A>]</C> where <A>G</A> is isomorphic to <C>SmallGroup( <A>order</A>, <A>i</A> )</C>.
+##  <Example><![CDATA[
+##  gap> IdSmallGroup( GL( 2,3 ) );
+##  [ 48, 29 ]
+##  gap> IdSmallGroup( Group( (1,2,3,4),(4,5) ) );
+##  [ 120, 34 ]
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
@@ -441,6 +445,26 @@ DeclareGlobalFunction( "IdStandardPresented512Group" );
 ##
 ##  <Description>
 ##  prints information on the groups of the specified order.
+##  <Example><![CDATA[
+##  gap> SmallGroupsInformation( 32 );
+##  
+##    There are 51 groups of order 32.
+##    They are sorted by their ranks. 
+##       1 is cyclic. 
+##       2 - 20 have rank 2.
+##       21 - 44 have rank 3.
+##       45 - 50 have rank 4.
+##       51 is elementary abelian. 
+##  
+##    For the selection functions the values of the following attributes 
+##    are precomputed and stored:
+##       IsAbelian, PClassPGroup, RankPGroup, FrattinifactorSize and 
+##       FrattinifactorId. 
+##  
+##    This size belongs to layer 2 of the SmallGroups library. 
+##    IdSmallGroup is available for this size. 
+##   
+##  ]]></Example>
 ##  </Description>
 ##  </ManSection>
 ##  <#/GAPDoc>
